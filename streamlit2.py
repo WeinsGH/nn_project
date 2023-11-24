32ab636b19bf40c73b8a9a3003991fee24643ec4
Solar-Iz <146209729+Solar-Iz@users.noreply.github.com>
2023-11-24 14:09:32 +0300

    Add files via upload
    Посмотри пожалуйста в части словаря и предсказаний.

--- streamlit2.py
+++ streamlit2.py
@@ -5,12 +5,27 @@ from torchvision import transforms
 from torchvision.models import resnet18
 from torch.autograd import Variable
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 # Загрузка предварительно обученной модели
 model = resnet18(pretrained=False)
 # Замените 'path/to/your/model.pth' на путь к вашим весам модели
 model.fc = nn.Linear(model.fc.in_features, 10)
 model.load_state_dict(torch.load('model/model.pth', map_location='cpu'))
+# Словарь сопоставления числовых предсказаний с классами
+class_mapping = {
+    0: "AnnualCrop",
+    1: "Forest",
+    2: "HerbaceousVegetation",
+    3: "Highway",
+    4: "Industrial",
+    5: "Pasture",
+    6: "PermanentCrop",
+    7: "Residential",
+    8: "River",
+    9: "SeaLake"
+}
+
 st.write(print(model.fc.weight))
 model.eval()
 
@@ -45,14 +60,33 @@ def main():
         st.write("")
         st.write("Classifying...")
 
-        # Преобразование изображения в объект PIL
+        
+        # # Классификация изображения
+        # predicted_class, confidence = classify_image(image, model)
+
+        # st.write(f"Prediction: Class {predicted_class}")
+        # st.write(f"Confidence: {confidence:.2%}")
+
+        # Предобработка изображения
+        image_tensor = preprocess_image(image).to('cpu')
+        ## Предсказание с помощью модели
+        with torch.no_grad():
+            outputs = model(image_tensor)
+
+        # Преобразование предсказаний в вероятности с использованием сигмоиды
+        predicted_probs = torch.sigmoid(outputs)
+
+        # Получение индекса предсказанного класса
+        _, predicted_classes = torch.max(predicted_probs, 1)
+        predicted_class_index = int(predicted_classes[0].item())
+
+        # Отображение изображения и подпись предсказанного класса
         image = Image.open(uploaded_image)
+        plt.imshow(image)
+        predicted_class_name = class_mapping.get(predicted_class_index, f'Class {predicted_class_index}')
+        plt.title(f'Predicted Class: {predicted_class_name}')
+        plt.show()
 
-        # Классификация изображения
-        predicted_class, confidence = classify_image(image, model)
-
-        st.write(f"Prediction: Class {predicted_class}")
-        st.write(f"Confidence: {confidence:.2%}")
-
+        
 if __name__ == "__main__":
     main()
